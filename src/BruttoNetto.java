24a7f55916297e3efce6ac7497030cd92acf7f2f
kbuhlert <kbuhlert@gmail.com>
2020-03-04 10:23:20 +0100

    SteuerMax Berechnung zugefügt

--- src/BruttoNetto.java
+++ src/BruttoNetto.java
@@ -1,9 +1,13 @@
 public class BruttoNetto {
     public static void main (String[] args){
         int nettopreis = 200;
-        float steuersatz = 0.05f;
+        float steuersatz = 0.08f;
         double bruttoPreis;
         bruttoPreis = nettopreis + nettopreis * steuersatz;
         System.out.println("Ergebnis: " + bruttoPreis + "€");
+        float steuerMax = 0.1f;
+        boolean steuerMaxTest;
+        steuerMaxTest = steuersatz < steuerMax;
+        System.out.println("Der Steuersatz übertrifft nicht das Max: " + steuerMaxTest);
     }
 }
