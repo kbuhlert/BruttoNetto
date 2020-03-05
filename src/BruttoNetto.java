f4c5ad3e68506476739f4691a47ac82606448d77
kbuhlert <kbuhlert@gmail.com>
2020-03-05 09:22:25 +0100

    Zahlenwertvergleich mit if else

--- src/BruttoNetto.java
+++ src/BruttoNetto.java
@@ -7,12 +7,26 @@ public class BruttoNetto {
         System.out.println("Ergebnis: " + bruttoPreis + "€");
         double steuerMax = 0.1;
         boolean steuerMaxTest;
-        steuerMaxTest = steuersatz > steuerMax;
+        steuerMaxTest = steuersatz < steuerMax;
         System.out.println("Der Steuersatz übertrifft das Max: " + steuerMaxTest);
 
-        if (steuerMaxTest = true){
+        if (steuerMaxTest = false){
             System.out.println("Achtung, der maximale Steuersatz ist überschritten");
         } else {
             System.out.println("Der gewählte Steuersatz ist korrekt");}
+
+        int z1 = 1;
+        int z2 = 2;
+        int z3 = 3;
+        int z4 = 4;
+        if (z1 == 2) {
+            System.out.println("Wert 1 = 2");
+        } else if (z2 == 2){
+            System.out.println("Wert 2 = 2");}
+        else if (z3 == 2){
+            System.out.println("Wert 3 = 2");}
+        else {
+            System.out.println("Werte 1 bis 3 sind nicht 2");}
+
     }
 }
